f8a8eb8bb31875161fa770ff628dfcc894e98540
Yu, Li-Yu <afg984@gmail.com>
2021-03-05 15:59:37 +0800

    js: class

--- static/index.js
+++ static/index.js
@@ -1,55 +1,80 @@
-const wpm = document.getElementById("wpm");
-const div = document.getElementById("div");
-const input = document.getElementById("input");
-function resetAnimation(element) {
-  // https://stackoverflow.com/a/45036752
-  element.style.animation = 'none';
-  element.offsetHeight; /* trigger reflow */
-  element.style.animation = null;
-}
-function setDiv(done, bad, todo) {
-  div.children[0].textContent = done;
-  div.children[1].textContent = bad;
-  div.children[2].textContent = todo[0];
-  resetAnimation(div.children[2]);
-  div.children[3].textContent = todo.slice(1);
-}
-var goodpos = 0;
-var wordpos = 0;
-var pos = 0;
-var t0 = null;
-function inputChanged(event) {
-  goodpos = wordpos;
-  for (let i = 0; i < input.value.length; i++) {
-    let c = input.value[i];
-    if (text[wordpos + i] == c) {
-      goodpos = wordpos + i + 1;
-    } else {
-      break;
-    }
+class Typer {
+  static resetAnimation(element) {
+    // https://stackoverflow.com/a/45036752
+    element.style.animation = 'none';
+    element.offsetHeight; /* trigger reflow */
+    element.style.animation = null;
   }
-  pos = wordpos + input.value.length;
-  if (text[goodpos - 1] == " " || goodpos == text.length) {
-    wordpos = goodpos;
-    input.value = "";
-    if (goodpos == text.length) {
-      input.disabled = true;
-    }
-  }
-  setDiv(text.slice(0, goodpos), text.slice(goodpos, pos), text.slice(pos));
 
-  if (t0 === null) {
-    t0 = Date.now();
+  static calculatePositions(text, wordPos, inputValue) {
+    let goodPos = wordPos;
+    for (let i = 0; i < inputValue.length; i++) {
+      if (text[wordPos + i] == inputValue[i]) {
+        goodPos = wordPos + i + 1;
+      } else {
+        break;
+      }
+    }
+    let pos = wordPos + inputValue.length;
+    let shouldClean = false;
+    if (text[goodPos - 1] == " " || goodPos == text.length) {
+      wordPos = goodPos;
+      shouldClean = true;
+    }
+
+    return {
+      wordPos: wordPos,
+      goodPos: goodPos,
+      pos: pos,
+      shouldClean: shouldClean,
+    }
   }
-  else if (goodpos > 0) {
-    wpm.textContent = Math.round(
-      Math.max(0, goodpos - 1) / 5  // 1 word = 5 chars
-      / (Date.now() - t0) * 60000
-    );
+
+  setDiv(done, bad, todo) {
+    this.div.children[0].textContent = done;
+    this.div.children[1].textContent = bad;
+    this.div.children[2].textContent = todo[0];
+    Typer.resetAnimation(this.div.children[2]);
+    this.div.children[3].textContent = todo.slice(1);
   }
-}
-input.addEventListener("input", inputChanged);
-window.onload = function initialize() {
-  setDiv("", "", text);
-  console.log("init");
+
+  updateWpm(n) {
+    if (this.t0 === null) {
+      this.t0 = Date.now();
+      return;
+    }
+    if (n > 0) {
+      this.wpm.textContent = Math.round((n - 1) / 5 * 6e4 / (Date.now() - this.t0));
+    }
+  }
+
+  constructor(text, div, input, wpm) {
+    this.text = text;
+    this.div = div;
+    this.input = input;
+    this.wpm = wpm;
+    this.t0 = null;
+    this.wordPos = 0;
+
+    this.setDiv("", "", text);
+    let self = this;  // do we need self?
+    input.addEventListener('input', (event) => {
+      let r = Typer.calculatePositions(text, self.wordPos, self.input.value);
+      console.log(r);
+      if (r.shouldClean) {
+        self.input.value = "";
+      }
+      self.wordPos = r.wordPos;
+      self.setDiv(
+        text.slice(0, r.goodPos),
+        text.slice(r.goodPos, r.pos),
+        text.slice(r.pos),
+      );
+      self.updateWpm(r.goodPos);
+
+      if (r.goodPos == self.text.length) {
+        self.input.disabled = true;
+      }
+    });
+  }
 }
